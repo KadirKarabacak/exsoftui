718c1f4325f5d25c902e5c2ee6919957e6790d66
KadirKarabacak <kadirht@hotmail.com>
2024-09-23 00:56:41 +0300

    feat: create IconButton stories to test flexibility

---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { IconButton } from "./IconButton";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: "Example/IconButton",
+    component: IconButton,
+    parameters: {
+        // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+        layout: "centered",
+    },
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+    tags: ["autodocs"],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {},
+    // Apply props to All Stories
+    args: { onClick: fn() },
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const PrimaryContained: Story = {
+    args: {
+        color: "primary",
+    },
+};
