e7eb79de971830c856b9f4e252fbef6c04d52ff7
KadirKarabacak <kadirht@hotmail.com>
2024-09-23 00:56:03 +0300

    feat: update button with sizes & isLoading & spinner

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { clsx } from "../utils/utils";
 import "./button.css";
-import { BeakerIcon } from "@heroicons/react/24/solid";
+// import { BeakerIcon } from "@heroicons/react/24/solid";
 
 //! Test Cases
-// = <BeakerIcon className="size-6" />
 
 export interface ButtonProps {
     variant?: "outlined" | "contained";
@@ -13,9 +13,13 @@ export interface ButtonProps {
     onClick?: () => void;
     className?: string;
     disabled?: boolean;
-    startIcon?: React.ReactNode;
     endIcon?: React.ReactNode;
     animated?: boolean;
+    textSize?: "small" | "medium" | "large";
+    fullWidth?: boolean;
+    isLoading?: boolean;
+    loadingText?: string;
+    spinnerColor?: string;
 }
 
 /** Primary UI component for user interaction */
@@ -24,36 +28,66 @@ export const Button = ({
     color = "primary",
     size = "medium",
     animated = true,
+    textSize = "medium",
+    fullWidth = false,
+    isLoading = false,
+    loadingText = "Loading...",
+    spinnerColor,
     label,
     className,
     disabled,
-    startIcon,
+    // endIcon = <BeakerIcon className="w-4 h-4" />,
     endIcon,
     ...props
 }: ButtonProps) => {
-    const isIcon = startIcon || endIcon;
+    //! Disabled
+    const getDisabledStyles = (color: string) => ({
+        backgroundColor:
+            variant !== "outlined"
+                ? `var(--${color}-color, var(--disabled-color))`
+                : "",
+        opacity: 0.8,
+    });
 
-    const buttonClass = `
-        exsoft-button 
-        exsoft-button--${size} 
-        exsoft-button--${variant} 
-        exsoft-button--${color} 
-        ${className || ""} 
-        ${disabled ? "exsoft-button--disabled" : ""} 
-        ${isIcon ? "gap-2 items-center" : ""} 
-        ${animated ? "exsoft-button--animated" : ""}
-    `;
+    const buttonClass = clsx(
+        "exsoft-button",
+        `exsoft-button--${size}`,
+        `exsoft-button--${variant}`,
+        `exsoft-button--${color}`,
+        `exsoft-button--text-${textSize}`,
+        disabled && "exsoft-button--disabled",
+        endIcon && "gap-2 items-center",
+        animated && "exsoft-button--animated",
+        fullWidth && "exsoft-button--fullWidth",
+        isLoading && "exsoft-button--loading",
+        className
+    );
+
+    const spinnerStyles = {
+        borderColor:
+            variant === "outlined"
+                ? "var(--loading-spinner-color)"
+                : "var(--spinner-border-color)",
+        borderTopColor:
+            variant === "outlined"
+                ? `var(--${color}-color)`
+                : "var(--loading-spinner-color)",
+    };
 
     return (
         <button
             type="button"
-            // Size - Variant - ClassName - Disabled - Icon - Animation
             className={buttonClass}
+            style={disabled ? getDisabledStyles(color) : {}}
+            aria-disabled={disabled || isLoading}
+            disabled={disabled || isLoading}
             {...props}
         >
-            {startIcon && startIcon}
-            {label}
-            {endIcon && endIcon}
+            {isLoading && (
+                <span className="loading-spinner" style={spinnerStyles}></span>
+            )}
+            {isLoading ? loadingText : label}
+            {!isLoading && endIcon}
         </button>
     );
 };
